e5e91ffcfd2a8c9818f878c9f7ba1f91c1e3a681
soaza <chukimguan@gmail.com>
2023-02-23 11:20:47 +0800

    fix

--- myshell.c
+++ myshell.c
@@ -139,10 +139,10 @@ static void command_exec(char program[],char *args[],int args_count) {
 
         // else : ex1, ex2 
         // call execv() to execute the command in the child process
-        char *argv[] = {program};
-        execv("/bin/ls",argv);
+        char *argv[] = {"/bin/echo", "hello", NULL};
+        execv("/bin/echo",argv);
         // execv(program,args);
-        printf("HIII\n");
+        // printf("HIII\n");
         for (int i = 0; i < args_count; i++){
             printf("args: %s\n", args[i]);
         }
